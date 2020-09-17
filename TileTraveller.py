4a1ff705d7a8dbd2f5630edffb4af0962348a783
fjola19 <fjola19@ru.is>
2020-09-17 14:21:15 +0000

    lýsing á algorithma

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,1 +1,18 @@
-print('hello')
+# Byrjum í reit 1,1 og eigum að enda í reit 3,1 
+# Áttir sem maður kemst í:
+# 1,1 : Norður
+# 1,2 : Norður, austur, suður
+# 1,3 : Austur, suður
+# 2,1 : Norður
+# 2,2 : Vestur, suður
+# 2,3 : Vestur, austur
+# 3,1 : Victory
+# 3,2 : Norður, suður
+# 3,3 : Vestur, suður
+
+# while lykkja utan um allt þangað til x=3 og y=1
+# eitt function fyrir núverandi staðsetingu til að segja hvað má gera næst
+# eitt function sem breytir staðsetingu
+# Nota tvær breytur x,y
+# Norður = y hækkar, Suður = y lækkar
+# Vestur = x lækkar, Austur = x hækkar
